2b64b30ba802665514f02ba53497ef4445140305
oblakofff <sobinnicov@gmail.com>
2019-05-26 17:50:03 +0700

    Update dump.sql and insert.sql

--- sql/dump.sql
+++ sql/dump.sql
@@ -1,74 +1,70 @@
-CREATE TABLE "User" (
+CREATE TABLE "users" (
 	"id" serial NOT NULL,
 	"first_name" TEXT NOT NULL,
-	"last_name" TEXT NOT NULL,
+	"second_name" TEXT NOT NULL,
 	"created_at" TIMESTAMP NOT NULL,
 	"last_visit" TIMESTAMP NOT NULL,
-	CONSTRAINT User_pk PRIMARY KEY ("id")
+	"is_blocked" BOOLEAN NOT NULL,
+	"is_active" BOOLEAN NOT NULL,
+	"is_deleted" BOOLEAN NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Message" (
+CREATE TABLE "messages" (
 	"id" serial NOT NULL,
 	"dialog_id" integer NOT NULL,
-	"content_id" integer NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
-	"to_id" integer NOT NULL,
-	CONSTRAINT Message_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "Content" (
-	"id" serial NOT NULL,
 	"content" TEXT NOT NULL,
-	CONSTRAINT Content_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "Dialog" (
-	"id" serial NOT NULL,
-	"name" TEXT NOT NULL,
 	"created_at" TIMESTAMP NOT NULL,
-	CONSTRAINT Dialog_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "DialogsUsers" (
-	"user_id" integer NOT NULL,
-	"dialog_id" integer NOT NULL,
-	"permission" integer
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "Ban_List" (
-	"id" serial NOT NULL,
 	"to_id" integer NOT NULL,
-	CONSTRAINT Ban_List_pk PRIMARY KEY ("id")
+	"section_id" integer NOT NULL,
+	CONSTRAINT messages_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "Authentication" (
+CREATE TABLE "dialogs" (
+	"id" serial NOT NULL,
+	"name" TEXT,
+	"created_at" TIMESTAMP NOT NULL,
+	CONSTRAINT dialogs_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "dialogUser" (
+	"dialog_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	"permisson" integer DEFAULT NULL
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "authentications" (
 	"user_id" integer NOT NULL,
 	"login" TEXT NOT NULL,
-	"password" TEXT NOT NULL
+	"password" TEXT NOT NULL,
+	CONSTRAINT authentications_pk PRIMARY KEY ("user_id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "relationships" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"status" integer,
+	CONSTRAINT relationships_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -76,16 +72,13 @@ CREATE TABLE "Authentication" (
 
 
 
-ALTER TABLE "Message" ADD CONSTRAINT "Message_fk0" FOREIGN KEY ("dialog_id") REFERENCES "Dialog"("id");
-ALTER TABLE "Message" ADD CONSTRAINT "Message_fk1" FOREIGN KEY ("content_id") REFERENCES "Content"("id");
-ALTER TABLE "Message" ADD CONSTRAINT "Message_fk2" FOREIGN KEY ("to_id") REFERENCES "User"("id");
+ALTER TABLE "messages" ADD CONSTRAINT "messages_fk0" FOREIGN KEY ("dialog_id") REFERENCES "dialogs"("id");
 
 
+ALTER TABLE "dialogUser" ADD CONSTRAINT "dialogUser_fk0" FOREIGN KEY ("dialog_id") REFERENCES "dialogs"("id");
+ALTER TABLE "dialogUser" ADD CONSTRAINT "dialogUser_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 
-ALTER TABLE "DialogsUsers" ADD CONSTRAINT "DialogsUsers_fk0" FOREIGN KEY ("user_id") REFERENCES "User"("id");
-ALTER TABLE "DialogsUsers" ADD CONSTRAINT "DialogsUsers_fk1" FOREIGN KEY ("dialog_id") REFERENCES "Dialog"("id");
+ALTER TABLE "authentications" ADD CONSTRAINT "authentications_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
 
-ALTER TABLE "Ban_List" ADD CONSTRAINT "Ban_List_fk0" FOREIGN KEY ("id") REFERENCES "User"("id");
-
-ALTER TABLE "Authentication" ADD CONSTRAINT "Authentication_fk0" FOREIGN KEY ("user_id") REFERENCES "User"("id");
+ALTER TABLE "relationships" ADD CONSTRAINT "relationships_fk0" FOREIGN KEY ("id") REFERENCES "users"("id");
 
