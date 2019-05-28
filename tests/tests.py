59dfc7dbae7de8e46b4103447d39c7974ab454f7
madwayz <djurball@list.ru>
2019-05-28 22:49:58 +0700

    * Fixed unittests

--- tests/tests.py
+++ tests/tests.py
@@ -2,38 +2,35 @@ import unittest
 import requests
 
 
-class TestUser(unittest.TestCase):
+class Test(unittest.TestCase):
 
-	def test_get_user(self)
-		for ID in range(10):
-			resp = requests.post('http://127.0.0.1/profile/{}'.format(ID), json=data)
-			self.assertEqual(resp.status_code, 200)
-			self.assertIsNotNone(resp.text)
-		
-		
-	def test_get_users(self)
-		resp = requests.post('http://127.0.0.1/profiles', json=data)
+    def get_user(self):
+        for ID in range(10):
+            resp = requests.get('http://127.0.0.1/profile/{}'.format(ID))
+            self.assertEqual(resp.status_code, 200)
+            self.assertIsNotNone(resp.text)
+
+    def get_users(self):
+        resp = requests.get('http://127.0.0.1/profiles')
         self.assertEqual(resp.status_code, 200)
         self.assertIsNotNone(resp.text)
-		
-		
-	def test_login_user(self):
-		data = {
-			'login': 'SergeyAstapov',
-			'password': 'SergeyAstapov123',
+
+    def login_user(self):
+        data = {
+            'login': 'SergeyAstapov',
+            'password': 'SergeyAstapov123',
         }
         resp = requests.post('http://127.0.0.1/login', json=data)
         self.assertEqual(resp.status_code, 200)
         self.assertIsNotNone(resp.text)
 
-
     def test_add_user(self):
         data = {
             'first_name': 'Sergey',
             'second_name': 'Astapov',
-			'login': 'SergeyAstapov',
-			'password': 'SergeyAstapov123',
-			'confirm_password': 'SergeyAstapov123',
+            'login': 'SergeyAstapov',
+            'password': 'SergeyAstapov123',
+            'confirm_password': 'SergeyAstapov123',
         }
         resp = requests.post('http://127.0.0.1/register', json=data)
         self.assertEqual(resp.status_code, 200)
