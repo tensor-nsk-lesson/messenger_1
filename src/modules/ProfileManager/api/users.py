efcde0418116758947beb0875ada32b6e89422d0
Павел Облаков <sobinnicov@mail.ru>
2019-05-31 18:07:12 +0700

    * Update users.py + app.py + database.py

--- src/modules/ProfileManager/api/users.py
+++ src/modules/ProfileManager/api/users.py
@@ -1,73 +1,64 @@
-from database import sql_execute
+from modules.database import sql_execute
 
 def db_addProfile(data):
-    sql="""
-        INSERT INTO users (first_name, second_name, created_at, last_visit, is_blocked, is_active, is_deleted) 
-        VALUES ('{first_name}', '{second_name}', NOW(), NOW(), false, true, false) RETURNING id;
-    """.format(**data)
-    userid = sql_execute(sql, True)
+    sql= ("INSERT INTO users (first_name, second_name, created_at, last_visit, is_blocked, is_active, is_deleted)\n"
+          "VALUES ('{first_name}', '{second_name}', NOW(), NOW(), false, true, false)\n"
+          "RETURNING id"
+          ).format(**data)
+    userid = sql_execute(sql)
     print(sql)
-    sql = """
-        INSERT INTO authentications (user_id, login, password) 
-        VALUES ('{}', '{login}', '{password}');
-    """.format(userid[0]['id'], **data)
+    sql = ("INSERT INTO authentications (user_id, login, password) \n"
+           "VALUES ({}, '{login}', '{password}')"
+           ).format(userid[0]['id'], **data)
     print(sql)
-    sql_execute(sql, True)
-
+    sql_execute(sql)
 
 def db_delProfile(ID):
-    # TODO: Добавить запрос на удаление пользователя
-    sql="""
-        UPDATE users 
-        SET is_deleted = true
-        WHERE id='{ID}';
-    """.format(ID)
-    return sql_execute(sql, True)
-
+    sql= ("UPDATE users\n"
+          "SET is_deleted = true\n"
+          "WHERE id = {ID}"
+          ).format(ID)
+    return sql_execute(sql)
 
 def db_isUserCreated(data):
-    sql="""
-        SELECT count(id) 
-        FROM authentications 
-        WHERE login='{login}'};
-    """.format(**data)
-    return sql_execute(sql, False) == 1
+    sql= ("SELECT count(user_id)\n"
+          "FROM authentications\n"
+          "WHERE login = '{login}'"
+          ).format(**data)
+    return True if sql_execute(sql, False) else False
 
-
-# TODO: Сделать ещё возвращение ID.
 def db_getProfileInfo(ID):
-    sql="""
-        SELECT first_name, last_name, created_at, last_visit 
-        FROM User
-        WHERE id='{ID}';
-    """.format(ID)
+    sql= ("SELECT id, first_name, second_name, created_at, last_visit\n"
+          "FROM users\n"
+          "WHERE id = {ID}"
+          ).format(ID)
     return sql_execute(sql, False)
 
+def db_getUserID(data):
+    sql= ("SELECT user_id\n"
+          "FROM authentications\n"
+          "where login='{login}'"
+          ).format(**data)
+    user_id = sql_execute(sql, False)
+    return user_id['user_id']
 
 def db_updateProfileInfo(ID, data):
     for key in data:
         if data[key]:
-            sql="""
-                UPDATE User
-                SET {1}='{2}' 
-                WHERE id='{3}';
-            """.format(key, data[key], ID)
+            sql= ("UPDATE users\n"
+                  "SET {1} = '{2}'\n"
+                  "WHERE id = {3}"
+                  ).format(key, data[key], ID)
     return sql_execute(sql, False)
-
 
 def db_getProfilesInfo():
-    sql="""
-        SELECT * 
-        FROM Users;
-    """
+    sql= ("SELECT * FROM users")
     return sql_execute(sql, False)
-
 
 def db_isProfileValid(data):
-    sql="""
-        SELECT count(user_id)
-        FROM authentications
-        WHERE login='{login}' 
-        AND password='{password}';
-    """.format(**data)
-    return sql_execute(sql, False)
+    sql= ("SELECT count(user_id)\n"
+          "FROM authentications\n"
+          "WHERE login='{login}'\n"
+          "AND password='{password}'"
+          ).format(**data)
+    return True if sql_execute(sql, False) else False
