92cdf1e88c635cb90c7090ba88dd395b60554717
Pavel Oblakov <32703677+oblakoFFF@users.noreply.github.com>
2019-06-03 02:05:53 +0700

    Update db_methods.py

--- src/modules/ProfileManager/api/db_methods.py
+++ src/modules/ProfileManager/api/db_methods.py
@@ -3,44 +3,83 @@ from modules.database import sql_execute
 
 ## DEVELOP METHODS
 def db_addProfile(data):
-    sql = ("INSERT INTO users (first_name, second_name, created_at, last_visit, is_blocked, is_online, is_deleted)\n"
-           "VALUES ('%(first_name)s', '%(second_name)s', NOW(), NOW(), false, true, false)\n"
-           "RETURNING id;"
-           ) % data
-    user_id = sql_execute(sql, False)
-    sql = ("INSERT INTO authentications (user_id, login, password)\n"
-           "VALUES (%d, '%(login)s', '%(password)s');"
-           ) % user_id[0]['id'], data
+    sql = (
+    """
+        WITH insert_users as (
+            INSERT INTO
+                users (
+                    first_name
+                    , second_name
+                    , created_at
+                    , last_visit
+                    , is_blocked
+                    , is_online
+                    , is_deleted
+                )
+            VALUES (
+                '%(first_name)s
+                , '%(second_name)s'
+                , NOW()
+                , NOW()
+                , false
+                , true
+                , false
+            )
+            RETURNING id
+        )
+        INSERT INTO
+            authentications (
+                user_id
+                , login
+                , password
+            )
+        VAlUES (
+            SELECT id FROM insert_users
+            , '%(login)s
+            , %(password)s
+        )
+    """
+    ) % data
     sql_execute(sql, False)
     return {'status': 1}
 
 
 def db_isAuthDataValid(data):
-    sql = ("SELECT user_id\n"
-           "FROM authentications\n"
-           "WHERE login='%(login)s' AND password='%(password)s';"
-           ) % data
+    sql = (
+    """
+        SELECT user_id
+        FROM authentications
+        WHERE login='%(login)s' AND password='%(password)s'
+    """           
+    ) % data
     answer = sql_execute(sql, False)
     return bool(answer['user_id'])
 
 
 def db_isProfileExists(data):
-    sql = ("SELECT count(login)\n"
-           "FROM authentications")
+    sql = (
+    """
+        SELECT count(login)
+        FROM authentications
+    """ 
+    )
     if type(data) == int:
         sql += "WHERE user_id='%d'" % data
     elif type(data) == dict:
-        sql += "WHERE login='$(login)s';" % data
+        sql += "WHERE login='$(login)s'" % data
 
     users = sql_execute(sql, False)['count']
     return bool(users)
 
 
 def db_setLastVisit(ID):
-    sql = ("UPDATE users\n"
-           "SET last_visit = NOW()\n"
-           "WHERE id='%d';"
-           ) % ID
+    sql = (
+    """
+        UPDATE users
+        SET last_visit = NOW()
+        WHERE id='%d'
+    """
+    ) % ID
     sql_execute(sql, False)
 
 
@@ -52,18 +91,24 @@ def db_setLastVisit(ID):
 
 
 def db_blockProfile(ID, status=True):
-    sql = ("UPDATE users\n"
-           "SET is_blocked='%s'\n"
-           "WHERE id='%d';"
-           ) % (status, ID)
+    sql = (
+    """
+        UPDATE users
+        SET is_blocked='%s'
+        WHERE id='%d'
+    """
+    ) % (status, ID)
     sql_execute(sql, False)
 
 
 def db_getUserID(data):
-    sql = ("SELECT user_id\n"
-           "FROM authentications\n"
-           "WHERE login='%(login)s';"
-           ) % data
+    sql = (
+    """ 
+        SELECT user_id
+        FROM authentications
+        WHERE login='%(login)s'
+    """
+    ) % data
     user_id = sql_execute(sql, False)
     return user_id['user_id']
 
@@ -77,36 +122,61 @@ def db_getUserID(data):
 
 
 def db_delProfile(ID, status=True):
-    sql = ("UPDATE users\n"
-           "SET is_deleted=%s\n"
-           "WHERE id=%d"
-           ) % (status, ID)
+    sql = (
+    """
+        UPDATE users
+        SET is_deleted=%s
+        WHERE id=%d
+    """
+    ) % (status, ID)
     return sql_execute(sql)
 
 
 def db_FullDelProfile(ID):
     # TODO: Добавить запрос на удаление пользователя
-    sql = ("DELETE FROM authentications\n"
-           "WHERE user_id='%d';\n"
-           "DELETE FROM users\n"
-           "WHERE id=\'%d\';"
-           ) % (ID, ID)
+    sql = (
+    """
+        DELETE FROM authentications
+        WHERE user_id='%d';
+        DELETE FROM users
+        WHERE id='%d'
+    """
+    ) % (ID, ID)
     sql_execute(sql, True)
     return {'status': 1}
 
 
 def db_getProfileInfo(ID):
-    sql = ("SELECT first_name, second_name, id, last_visit, is_deleted, is_blocked\n"
-           "FROM users\n"
-           "WHERE id='%d';"
-           ) % ID
+    sql = (
+    """
+        SELECT 
+            first_name
+            , second_name
+            , id
+            , last_visit
+            , is_deleted
+            , is_blocked
+        FROM users
+        WHERE id='%d'
+    """
+    ) % ID
     return sql_execute(sql, False)
 
 
 def db_getProfilesInfo():
-    sql = ("SELECT first_name, second_name, id, last_visit, is_deleted, is_blocked, is_online\n"
-           "FROM users;"
-           )
+    sql = (
+    """
+        SELECT 
+            first_name
+            , second_name
+            , id
+            , last_visit
+            , is_deleted
+            , is_blocked
+            , is_online
+        FROM users
+    """
+    )
     return sql_execute(sql)
 
 
@@ -117,20 +187,28 @@ def db_updateProfileInfo(ID, data):
             return {'status': 0, 'message': 'Неизвестное поле. Менять можно только first_name/second_name'}
 
         if data[key]:
-            sql = ("SELECT first_name, second_name\n"
-                   "FROM users\n"
-                   "WHERE id='%d';"
-                   ) % ID
+            sql = (
+            """
+                SELECT 
+                    first_name
+                    , second_name
+               FROM users
+               WHERE id='%d'
+            """
+            ) % ID
             answer = sql_execute(sql, False)
 
-            if data[key] == answer[key]:  # Если введённое и из БД поля эквиваленты, то выкидываем ошибку.
+            if data[key] == answer[key]: # Если введённое и из БД поля эквиваленты, то выкидываем ошибку.
                 rows.append(key)
                 continue
 
-            sql = ("UPDATE users\n"
-                   "SET %s='%s' \n"
-                   "WHERE id='%d';"
-                   ) % (key, data[key], ID)
+            sql = (
+            """
+                UPDATE users
+                SET %s='%s'
+                WHERE id='%d'
+            """
+            ) % (key, data[key], ID)
             sql_execute(sql, False)
 
     if not len(rows):
