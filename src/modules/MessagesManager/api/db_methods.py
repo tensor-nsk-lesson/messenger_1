1bdbadbc4d70262a7c88aaf09d7939a1d04e886d
Pavel Oblakov <32703677+oblakoFFF@users.noreply.github.com>
2019-06-03 02:11:06 +0700

    Update db_methods.py

--- src/modules/MessagesManager/api/db_methods.py
+++ src/modules/MessagesManager/api/db_methods.py
@@ -1,27 +1,72 @@
 from modules.database import sql_execute;
 
 def db_addDialog(nameDialog):
-    sql = ("INSERT INTO dialogs (name, created_at)\n"
-           "VALUES ('%s', NOW())"
-           ) % nameDialog
+    sql = (
+    """
+        INSERT INTO 
+            dialogs (
+                name
+                , created_at
+            )
+        VALUES (
+            '%s'
+            , NOW()
+        )
+    """
+    ) % nameDialog
     return {'status': 1}
 
 def db_addUserInDialog(userID, dialogID, permission):
-    sql = ("INSERT INTO dialogUser (dialog_id, user_id, permission)\n"
-           "VALUES (%d, %d, %d)"
-           ) % dialogID, userID, permission
+    sql = (
+    """
+        INSERT INTO 
+            dialogUser (
+                dialog_id
+                , user_id
+                , permission
+            )
+        VALUES (
+            %d
+            , %d
+            , %d
+        )
+    """
+    ) % dialogID, userID, permission
     return {'status': 1}
 
 
 def db_addMessageForDialog(userID, content, dialogID, section_id=0):
-    sql = ("INSERT INTO messages (dialog_id, content, created_at, user_id, section_id)\n"
-           "VALUES (%d, '%s', NOW(), %d, %d)"
-           ) % (dialogID, content, userID, section_id)
+    sql = (
+    """
+        INSERT INTO 
+            messages (
+                dialog_id
+                , content
+                , created_at
+                , user_id
+                , section_id
+            )
+        VALUES (
+            %d
+            , '%s'
+            , NOW()
+            , %d
+            , %d
+        )
+    """
+    ) % (dialogID, content, userID, section_id)
     return {'status': 1}
 
 def db_getMessagesFromDialog(dialogID):
-    sql = ("SELECT user_id, content, created_at, section_id\n"
-           "FROM messages\n"
-           "WHERE dialog_id='%s'"
-           ) % dialogID
+    sql = (
+    """
+        SELECT 
+            user_id
+            , content
+            , created_at
+            , section_id
+        FROM messages
+        WHERE dialog_id='%s'
+    """
+    ) % dialogID
     return sql_execute(sql)
