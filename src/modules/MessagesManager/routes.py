67f3db35d5b3f2a7ff09aab6ca3ab56601cb2576
madwayz <djurball@list.ru>
2019-06-12 15:31:55 +0700

    * Added rollback for function sql_execute()

--- src/modules/MessagesManager/routes.py
+++ src/modules/MessagesManager/routes.py
@@ -8,7 +8,7 @@ from modules.json_schemas import conference_create_schema
 
 messages_module = Blueprint('messages', __name__)
 
-@messages_module.route('/<int:dialogID>', methods=['GET, PUT, DELETE'])
+@messages_module.route('/<int:chatID>', methods=['GET, PUT, DELETE'])
 def send_message(chatID):
     if request.method == 'PUT':
         data = json_validate(request.data, conference_create_schema)
@@ -18,7 +18,7 @@ def send_message(chatID):
             return jsonify({'status': 0, 'message': 'Требуется запрос с JSON\'ом'})
 
         if not data['login'] or not data['content'] or not data['section_id']:
-            return jsonify({'status': 0, 'message': 'Не заполнены данные о имени конференции'})
+            return jsonify({'status': 0, 'message': 'Не заполнено имя конференции'})
 
         user_id = getUserID(UUID)
         return jsonify(db_addMessageForChat(user_id, data['content'], chatID, 0))
